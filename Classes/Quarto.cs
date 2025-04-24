f44483e56cb13bc4d0f778e82a2d925b6b77f5e1
goncalo <goncalopinto13032004@gmail.com>
2025-04-24 21:59:03 +0100

    Add Quarto class with properties and methods
    Introduced private fields for room state management in the `Quarto` class, along with public properties for controlled access and validation. Added a constructor and two methods, `alterarStatus` and `alterarOcupacao`, with noted logical errors in comparisons. Implemented a placeholder method `alterarConta` for cost calculation. Comments added for future enhancements and error handling.

--- Classes/Quarto.cs
+++ Classes/Quarto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,98 @@ namespace appBugInn
 {
     internal class Quarto
     {
+
+        private int _numQuarto;
+        private int _andar;
+        private float _conta;
+        private bool _livre;
+        private string _status;
+
+       public int NumQuarto
+        {
+            get => _numQuarto;
+            set
+            {
+                if (value < 100)
+                {
+                    //alguma coisa
+                }
+                _numQuarto = value;
+            }
+        }
+
+       public int Andar
+        {
+            get => _andar;
+            set
+            {
+                if (value < 0)
+                {
+                    //receber algo}
+                }
+                _andar = value;
+            }
+        }
+
+       public float Conta
+        {
+            get => _conta;
+            set
+            {
+                if (_conta < 0)
+                {
+                    //algo
+                }
+                _conta = value;
+            }
+        }
+       public bool Livre 
+        { get => _livre;
+            set
+            {
+                if (_livre = false)
+                {
+
+                }
+                _livre = value;
+            }
+        }
+
+       public string Status
+        {
+            get => _status;
+            set
+            {
+                if(_status != value)
+                {
+                    //
+                }
+                _status = value;
+            }
+        }
+        //criar lista de produtos para o minibar.
+
+
+        public Quarto(int numQuarto, int andar) { }
+
+        void alterarStatus() { }
+
+        bool alterarOcupacao(bool opLivre) 
+        {
+            if (opLivre = true) 
+            { 
+                return Livre = true; 
+            }
+            else { return Livre = false; }
+        }
+
+        double alterarConta(int dias, int consumiveis) 
+        {
+            //dias * preco do quarto + consumiveis + reparacoes
+            return 0;
+        }
+
+
+        
     }
 }
