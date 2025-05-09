9b86702e1f7789de1d3b43885c4d04257eaa2828
Helom Valentim <helom.valentim.t0127856@edu.atec.pt>
2025-05-09 19:26:41 +0100

    Enhance UI components in F_telaInicial
    Redefine `iconesMenu` ImageList and restore image keys.
    Increase sizes of several `TabPage` controls for better
    visibility. Adjust `tb_configuracoes` tab location and
    add `MaterialSwitch` for dark mode functionality.
    Update `sw_darkMode` size and position.
    Modify `F_telaInicial.resx` to include updated image stream.

--- F_telaInicial.Designer.cs
+++ F_telaInicial.Designer.cs
@@ -33,7 +33,6 @@ namespace appBugInn
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tb_home = new System.Windows.Forms.TabPage();
             this.tb_reservas = new System.Windows.Forms.TabPage();
-            this.iconesMenu = new System.Windows.Forms.ImageList(this.components);
             this.tb_checkIn = new System.Windows.Forms.TabPage();
             this.tb_checkOut = new System.Windows.Forms.TabPage();
             this.tb_gerirQuartos = new System.Windows.Forms.TabPage();
@@ -41,7 +40,9 @@ namespace appBugInn
             this.tb_diretoria = new System.Windows.Forms.TabPage();
             this.tb_configuracoes = new System.Windows.Forms.TabPage();
             this.sw_darkMode = new MaterialSkin.Controls.MaterialSwitch();
+            this.iconesMenu = new System.Windows.Forms.ImageList(this.components);
             this.materialTabControl1.SuspendLayout();
+            this.tb_configuracoes.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -82,10 +83,87 @@ namespace appBugInn
             this.tb_reservas.Location = new System.Drawing.Point(4, 39);
             this.tb_reservas.Name = "tb_reservas";
             this.tb_reservas.Padding = new System.Windows.Forms.Padding(3);
-            this.tb_reservas.Size = new System.Drawing.Size(786, 340);
+            this.tb_reservas.Size = new System.Drawing.Size(961, 512);
             this.tb_reservas.TabIndex = 1;
             this.tb_reservas.Text = "RESERVAS";
             this.tb_reservas.UseVisualStyleBackColor = true;
+            // 
+            // tb_checkIn
+            // 
+            this.tb_checkIn.ImageKey = "icons8-check-in-hotel-32.png";
+            this.tb_checkIn.Location = new System.Drawing.Point(4, 39);
+            this.tb_checkIn.Name = "tb_checkIn";
+            this.tb_checkIn.Size = new System.Drawing.Size(961, 512);
+            this.tb_checkIn.TabIndex = 2;
+            this.tb_checkIn.Text = "CHECK IN";
+            this.tb_checkIn.UseVisualStyleBackColor = true;
+            // 
+            // tb_checkOut
+            // 
+            this.tb_checkOut.ImageKey = "icons8-hotel-chekc-fora-32.png";
+            this.tb_checkOut.Location = new System.Drawing.Point(4, 39);
+            this.tb_checkOut.Name = "tb_checkOut";
+            this.tb_checkOut.Size = new System.Drawing.Size(961, 512);
+            this.tb_checkOut.TabIndex = 3;
+            this.tb_checkOut.Text = "CHECK OUT";
+            this.tb_checkOut.UseVisualStyleBackColor = true;
+            // 
+            // tb_gerirQuartos
+            // 
+            this.tb_gerirQuartos.ImageKey = "icons8-quarto-32.png";
+            this.tb_gerirQuartos.Location = new System.Drawing.Point(4, 39);
+            this.tb_gerirQuartos.Name = "tb_gerirQuartos";
+            this.tb_gerirQuartos.Size = new System.Drawing.Size(961, 512);
+            this.tb_gerirQuartos.TabIndex = 4;
+            this.tb_gerirQuartos.Text = "GERIR QUARTOS";
+            this.tb_gerirQuartos.UseVisualStyleBackColor = true;
+            // 
+            // tb_funcionarios
+            // 
+            this.tb_funcionarios.ImageKey = "icons8-contrato-de-trabalho-32.png";
+            this.tb_funcionarios.Location = new System.Drawing.Point(4, 39);
+            this.tb_funcionarios.Name = "tb_funcionarios";
+            this.tb_funcionarios.Size = new System.Drawing.Size(961, 512);
+            this.tb_funcionarios.TabIndex = 5;
+            this.tb_funcionarios.Text = "FUNCIONARIOS";
+            this.tb_funcionarios.UseVisualStyleBackColor = true;
+            // 
+            // tb_diretoria
+            // 
+            this.tb_diretoria.ImageKey = "icons8-gerente-32.png";
+            this.tb_diretoria.Location = new System.Drawing.Point(4, 39);
+            this.tb_diretoria.Name = "tb_diretoria";
+            this.tb_diretoria.Size = new System.Drawing.Size(961, 512);
+            this.tb_diretoria.TabIndex = 6;
+            this.tb_diretoria.Text = "DIRETORIA";
+            this.tb_diretoria.UseVisualStyleBackColor = true;
+            // 
+            // tb_configuracoes
+            // 
+            this.tb_configuracoes.Controls.Add(this.sw_darkMode);
+            this.tb_configuracoes.ImageKey = "icons8-config-32.png";
+            this.tb_configuracoes.Location = new System.Drawing.Point(4, 39);
+            this.tb_configuracoes.Name = "tb_configuracoes";
+            this.tb_configuracoes.Size = new System.Drawing.Size(961, 512);
+            this.tb_configuracoes.TabIndex = 7;
+            this.tb_configuracoes.Text = "CONFIGURAÇÕES";
+            this.tb_configuracoes.UseVisualStyleBackColor = true;
+            // 
+            // sw_darkMode
+            // 
+            this.sw_darkMode.BackColor = System.Drawing.Color.Transparent;
+            this.sw_darkMode.Depth = 0;
+            this.sw_darkMode.Location = new System.Drawing.Point(36, 33);
+            this.sw_darkMode.Margin = new System.Windows.Forms.Padding(0);
+            this.sw_darkMode.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.sw_darkMode.MouseState = MaterialSkin.MouseState.HOVER;
+            this.sw_darkMode.Name = "sw_darkMode";
+            this.sw_darkMode.Ripple = true;
+            this.sw_darkMode.Size = new System.Drawing.Size(202, 44);
+            this.sw_darkMode.TabIndex = 1;
+            this.sw_darkMode.Text = "Modo Escuro";
+            this.sw_darkMode.UseVisualStyleBackColor = false;
+            this.sw_darkMode.CheckedChanged += new System.EventHandler(this.sw_darkMode_CheckedChanged);
             // 
             // iconesMenu
             // 
@@ -100,87 +178,11 @@ namespace appBugInn
             this.iconesMenu.Images.SetKeyName(6, "icons8-casa-32.png");
             this.iconesMenu.Images.SetKeyName(7, "icons8-contrato-de-trabalho-32.png");
             // 
-            // tb_checkIn
-            // 
-            this.tb_checkIn.ImageKey = "icons8-check-in-hotel-32.png";
-            this.tb_checkIn.Location = new System.Drawing.Point(4, 39);
-            this.tb_checkIn.Name = "tb_checkIn";
-            this.tb_checkIn.Size = new System.Drawing.Size(786, 340);
-            this.tb_checkIn.TabIndex = 2;
-            this.tb_checkIn.Text = "CHECK IN";
-            this.tb_checkIn.UseVisualStyleBackColor = true;
-            // 
-            // tb_checkOut
-            // 
-            this.tb_checkOut.ImageKey = "icons8-hotel-chekc-fora-32.png";
-            this.tb_checkOut.Location = new System.Drawing.Point(4, 39);
-            this.tb_checkOut.Name = "tb_checkOut";
-            this.tb_checkOut.Size = new System.Drawing.Size(786, 340);
-            this.tb_checkOut.TabIndex = 3;
-            this.tb_checkOut.Text = "CHECK OUT";
-            this.tb_checkOut.UseVisualStyleBackColor = true;
-            // 
-            // tb_gerirQuartos
-            // 
-            this.tb_gerirQuartos.ImageKey = "icons8-quarto-32.png";
-            this.tb_gerirQuartos.Location = new System.Drawing.Point(4, 39);
-            this.tb_gerirQuartos.Name = "tb_gerirQuartos";
-            this.tb_gerirQuartos.Size = new System.Drawing.Size(786, 340);
-            this.tb_gerirQuartos.TabIndex = 4;
-            this.tb_gerirQuartos.Text = "GERIR QUARTOS";
-            this.tb_gerirQuartos.UseVisualStyleBackColor = true;
-            // 
-            // tb_funcionarios
-            // 
-            this.tb_funcionarios.ImageKey = "icons8-contrato-de-trabalho-32.png";
-            this.tb_funcionarios.Location = new System.Drawing.Point(4, 39);
-            this.tb_funcionarios.Name = "tb_funcionarios";
-            this.tb_funcionarios.Size = new System.Drawing.Size(786, 340);
-            this.tb_funcionarios.TabIndex = 5;
-            this.tb_funcionarios.Text = "FUNCIONARIOS";
-            this.tb_funcionarios.UseVisualStyleBackColor = true;
-            // 
-            // tb_diretoria
-            // 
-            this.tb_diretoria.ImageKey = "icons8-gerente-32.png";
-            this.tb_diretoria.Location = new System.Drawing.Point(4, 39);
-            this.tb_diretoria.Name = "tb_diretoria";
-            this.tb_diretoria.Size = new System.Drawing.Size(786, 340);
-            this.tb_diretoria.TabIndex = 6;
-            this.tb_diretoria.Text = "DIRETORIA";
-            this.tb_diretoria.UseVisualStyleBackColor = true;
-            // 
-            // tb_configuracoes
-            // 
-            this.tb_configuracoes.ImageKey = "icons8-config-32.png";
-            this.tb_configuracoes.Location = new System.Drawing.Point(4, 74);
-            this.tb_configuracoes.Name = "tb_configuracoes";
-            this.tb_configuracoes.Size = new System.Drawing.Size(786, 305);
-            this.tb_configuracoes.TabIndex = 7;
-            this.tb_configuracoes.Text = "CONFIGURAÇÕES";
-            this.tb_configuracoes.UseVisualStyleBackColor = true;
-            // 
-            // sw_darkMode
-            // 
-            this.sw_darkMode.BackColor = System.Drawing.Color.Transparent;
-            this.sw_darkMode.Depth = 0;
-            this.sw_darkMode.Location = new System.Drawing.Point(871, 24);
-            this.sw_darkMode.Margin = new System.Windows.Forms.Padding(0);
-            this.sw_darkMode.MouseLocation = new System.Drawing.Point(-1, -1);
-            this.sw_darkMode.MouseState = MaterialSkin.MouseState.HOVER;
-            this.sw_darkMode.Name = "sw_darkMode";
-            this.sw_darkMode.Ripple = true;
-            this.sw_darkMode.Size = new System.Drawing.Size(52, 37);
-            this.sw_darkMode.TabIndex = 1;
-            this.sw_darkMode.UseVisualStyleBackColor = false;
-            this.sw_darkMode.CheckedChanged += new System.EventHandler(this.sw_darkMode_CheckedChanged);
-            // 
             // F_telaInicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(975, 622);
-            this.Controls.Add(this.sw_darkMode);
             this.Controls.Add(this.materialTabControl1);
             this.DrawerShowIconsWhenHidden = true;
             this.DrawerTabControl = this.materialTabControl1;
@@ -188,6 +190,7 @@ namespace appBugInn
             this.Text = "The Bug Inn";
             this.Load += new System.EventHandler(this.F_telaInicial_Load);
             this.materialTabControl1.ResumeLayout(false);
+            this.tb_configuracoes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
